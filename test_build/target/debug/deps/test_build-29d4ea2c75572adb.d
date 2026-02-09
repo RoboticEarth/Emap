312a3da6005c493fa5bc80b655850276116425da
robotic-earth <robotic-earth@protonmail.com>
2026-02-08 20:03:40 -0800

    oo

--- test_build/target/debug/deps/test_build-29d4ea2c75572adb.d
+++
@@ -1,5 +0,0 @@
-/home/tristan/Documents/roboric earth/code/Emap/test_build/target/debug/deps/test_build-29d4ea2c75572adb.d: src/main.rs
-
-/home/tristan/Documents/roboric earth/code/Emap/test_build/target/debug/deps/test_build-29d4ea2c75572adb: src/main.rs
-
-src/main.rs:
--- test_build/target/debug/test_build.d
+++
@@ -1,1 +0,0 @@
-/home/tristan/Documents/roboric\ earth/code/Emap/test_build/target/debug/test_build: /home/tristan/Documents/roboric\ earth/code/Emap/test_build/src/main.rs
